87d9b703a76638f433f194669b783515de90d9c3
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-10-02 19:57:51 +0530

    cloudinary photo upload

--- server/src/controllers/admin/news/createNews.controller.js
+++ server/src/controllers/admin/news/createNews.controller.js
@@ -1,11 +1,17 @@
 import { asyncHandler } from "../../../error/asyncHandler.error.js";
 import ErrorHandler from "../../../error/ErrorHandler.error.js";
 import { newsModels } from "../../../models/newsModels.js";
+import { fileUploadOnCloudinary } from "../../../utils/cloudinary.config.js";
 
 export const cerateNews = asyncHandler(async (req , res , next) => {
     const {title , content , summary , category } = req.body;
 
     const newsData = {title , content , summary , category , id : req.user.id}
+    let image;
+    console.log(req.file.path)
+    const res = fileUploadOnCloudinary(req.file.path)
+    if(res == null) return next(new ErrorHandler("Image upload failed ",400))
+    req.body.image = res.url;
     
     const news = await newsModels.create(req.body)
     if(!news) return next(new ErrorHandler("News creation failed !",402))
